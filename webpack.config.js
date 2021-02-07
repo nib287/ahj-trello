926d25f992fc11ca12630426445a9490273f3b84
Igor Nikolauk <nib287@yandex.ru>
2021-02-07 19:47:03 +0300

    fix: delete test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -22,11 +21,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].css"
         }),
-        // new CopyPlugin({
-        //     patterns: [
-        //       { from: './src/img', to: path.resolve(__dirname, 'dist/img') },
-        //     ],
-        // }),  
     ],
     
     devServer: {
