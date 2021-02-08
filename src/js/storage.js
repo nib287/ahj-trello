48138b23e788e868dbd30a89d8736e17e939d82f
Igor Nikolauk <nib287@yandex.ru>
2021-02-08 22:23:59 +0300

    fix: saveInlocalStorage()

--- src/js/storage.js
+++ src/js/storage.js
@@ -7,13 +7,13 @@ export default class Storage {
         }
     }
     
-    saveInlocalStorage() {
-        localStorage.setItem('data', JSON.stringify(this.cardsBox));
-    }
+    // saveInlocalStorage() {
+    //     localStorage.setItem('data', JSON.stringify(this.cardsBox));
+    // }
 
-    getdataInlocalStorage() {
-        const data = JSON.parse(localStorage.data);
-        if(data) this.cardsBox = data;
+    // getdataInlocalStorage() {
+    //     const data = JSON.parse(localStorage.data);
+    //     if(data) this.cardsBox = data;
         
-    }
+    // }
 }
